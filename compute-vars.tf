b69b45b8bc00fcd382d7ef23229107e0b9564d01
initaman <amandabral2104@gmail.com>
2025-12-01 14:44:41 +0530

    feat(EC2-Infra): Added ec2 and security-group infra

---
+++ compute-vars.tf
@@ -0,0 +1,15 @@
+variable "ec2_instance" {
+  description = "EC2 configuration"
+  type = map(object({
+    instance_type = string
+    subnet_name   = string
+    volume_size   = number
+    volume_type   = string
+  }))
+}
+
+variable "allowed_ssh_cidr" {
+  description = "A secure IPs for SSH-Connection"
+  type = list(string)
+  default = [ "" ]
+}
